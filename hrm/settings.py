2571d320d033f1708fee6bb86de00ce309a9d55d
ziaurjoy <ziaurjoy802@gmail.com>
2022-10-19 13:23:18 +0600

    postgresql setup

--- hrm/settings.py
+++ hrm/settings.py
@@ -73,12 +73,31 @@ WSGI_APPLICATION = 'hrm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hrm',
+        'USER': 'admin',
+        'PASSWORD': 'joy',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+
+
+
+
+
 
 
 # Password validation
